371df4e589769225257486dd82f11e16a4723a57
ZakryDat <zachary.datson@bbc.co.uk>
2024-06-05 11:25:52 +0100

    styling: improved header styling

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ import CarbonPerDayBarChart from './carbonPerDayChart';
 
 Chart.register(CategoryScale);
 
-let totalCarbonEmissionsToDate = 0;
+let sunakTotalCarbonEmissionsToDate = 0;
 SunakData.forEach(entry => {
     for (let trip in entry.trips) {
-        totalCarbonEmissionsToDate += entry.trips[trip];
+        sunakTotalCarbonEmissionsToDate += entry.trips[trip];
     }
 });
-totalCarbonEmissionsToDate = Math.round(totalCarbonEmissionsToDate)
+sunakTotalCarbonEmissionsToDate = Math.round(sunakTotalCarbonEmissionsToDate)
 
 let starmerTotalCarbonEmissionsToDate = 0;
 StarmerData.forEach(entry => {
@@ -27,36 +27,40 @@ function App() {
 
     return (
         <>
-            <div className='flex justify-between items-center py-4 px-6 font-semibold text-left'>
-                <div className='p-4'>
-                    <h1>Campaign</h1>
-                    <h1>Carbon</h1>
-                    <h1>Emissions</h1>
+            <div className='flex justify-between px-6 font-semibold mb-4 pb-6'>
+                <div className='flex items-baseline gap-8'>
+                    <h1>UKGE 2024</h1>
+                    <h2 className='text-3xl'>Campaign Carbon Emissions Tracker</h2>
                 </div>
-                <div className='text-4xl bg-gray-700 p-4 rounded-lg'>
-                    <h2 className='text-2xl pb-4'>Totals to date:</h2>
-                    <h2 className='pb-4'><span className='text-blue-500'>Sunak</span>: {totalCarbonEmissionsToDate} kg CO2e, or {totalCarbonEmissionsToDate / 0.04} cups of tea ☕</h2>
-                    <h2><span className='text-red-500'>Starmer</span>: {starmerTotalCarbonEmissionsToDate} kg CO2e or {starmerTotalCarbonEmissionsToDate / 0.04} cups of tea ☕</h2>
+                <div>
+                    <a href=''></a>
+                </div>
+            </div>
+            <div className='flex font-semibold items-center justify-center mb-6 gap-8'>
+                <div className='flex flex-col text-4xl bg-gray-400 dark:bg-gray-700 p-4 rounded-lg gap-2'>
+                    <h2 className='text-2xl text-left'>Totals to date:</h2>
+                    <div className='flex justify-between'>
+                        <h2 className='text-left text-blue-500'>Sunak:</h2>
+                        <h2 className='text-right'>{sunakTotalCarbonEmissionsToDate} kg CO2e</h2>
+                    </div>
+                    <h2><span className='text-red-500'>Starmer:</span> {starmerTotalCarbonEmissionsToDate} kg CO2e</h2>
+                </div>
+                <div className='flex flex-col text-2xl bg-blue-500 p-4 rounded-lg gap-2 text-right'>
+                    <h2>That's a combined total of</h2>
+                    <h2><span className='text-4xl'>{sunakTotalCarbonEmissionsToDate + starmerTotalCarbonEmissionsToDate}</span> kg CO2e,</h2>
+                    <h2>or <span className='text-4xl'>{(sunakTotalCarbonEmissionsToDate + starmerTotalCarbonEmissionsToDate) / 0.04}</span> cups of tea ☕</h2>
                 </div>
             </div>
             <div>
                 <CarbonPerDayBarChart />
             </div>
-            <div className='p-4'>
+            <div className='flex flex-col p-4 gap-2 text-left'>
                 <p className='font-semibold'>How are emissions calculated?</p>
-                <ul className='text-left pl-4 pt-2'>
-                    <li className='p-2'>
-                        By following Sunak & Starmer in the news, I have done the best I can to track their travel between locations.
-                    </li>
-                    <li className='p-2'>
-                        C02e is equal to the duration of each trip, multiplied by the typical fuel consumption for the vehicle in question
-                        and multiplied again by a literature-determined factor for converting fuel burn to carbon emissions (3.16 for aircraft).
-                    </li>
-                    <li className='p-2'>
-                        For air travel, I have tried to identify flights in question on flightradar24. For travel overland, I have assumed (where
-                        data is lacking) that if there is a direct train connection, then the candidates took the train, else they drove.
-                    </li>
-                </ul>
+                <p>By following Sunak & Starmer in the news, I have done the best I can to track their travel between locations.</p>
+                <p>C02e is equal to the duration of each trip, multiplied by the typical fuel consumption for the vehicle in question
+                    and multiplied again by a literature-determined factor for converting fuel burn to carbon emissions (3.16 for aircraft).</p>
+                <p>For air travel, I have tried to identify flights in question on flightradar24. For travel overland, I have assumed (where
+                    data is lacking) that if there is a direct train connection, then the candidates took the train, else they drove.</p>
             </div>
         </>
     )
